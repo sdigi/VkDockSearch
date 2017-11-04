11a6745f4375f619f4e1fb8a734a409d4438f8a2
AlKrot <almaz9595@bk.ru>
2017-11-04 16:16:45 +0300

    push

---
+++ VkDockSearch/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace VkDockSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tUserId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tDocIDStart = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tDocIDEnd = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.progressDoc = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tUserId
+            // 
+            this.tUserId.Location = new System.Drawing.Point(123, 12);
+            this.tUserId.Name = "tUserId";
+            this.tUserId.Size = new System.Drawing.Size(170, 20);
+            this.tUserId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID пользователя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Начальный ID дока";
+            // 
+            // tDocIDStart
+            // 
+            this.tDocIDStart.Location = new System.Drawing.Point(123, 38);
+            this.tDocIDStart.Name = "tDocIDStart";
+            this.tDocIDStart.Size = new System.Drawing.Size(170, 20);
+            this.tDocIDStart.TabIndex = 2;
+            this.tDocIDStart.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Конечный ID дока";
+            // 
+            // tDocIDEnd
+            // 
+            this.tDocIDEnd.Location = new System.Drawing.Point(123, 64);
+            this.tDocIDEnd.Name = "tDocIDEnd";
+            this.tDocIDEnd.Size = new System.Drawing.Size(170, 20);
+            this.tDocIDEnd.TabIndex = 4;
+            this.tDocIDEnd.Text = "999999";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(12, 90);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(281, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Найти";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // progressDoc
+            // 
+            this.progressDoc.Location = new System.Drawing.Point(15, 119);
+            this.progressDoc.Name = "progressDoc";
+            this.progressDoc.Size = new System.Drawing.Size(278, 23);
+            this.progressDoc.Step = 1;
+            this.progressDoc.TabIndex = 7;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 148);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(303, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 170);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.progressDoc);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tDocIDEnd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tDocIDStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tUserId);
+            this.Name = "Form1";
+            this.Text = "VkDockSearch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tUserId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tDocIDStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tDocIDEnd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ProgressBar progressDoc;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ VkDockSearch/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VkDockSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int userID, docStarId, docEndId;
+
+                if (int.TryParse(tUserId.Text, out userID))
+                {
+                    if (int.TryParse(tDocIDStart.Text, out docStarId) && int.TryParse(tDocIDEnd.Text, out docEndId))
+                    {
+                        int[] param = { userID, docStarId, docEndId };
+                        await ParsingDocUserIdAync(param);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Укажите ID пользователя");
+                }
+            }catch(Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
+        private async Task ParsingDocUserIdAync(object inParam)
+        {
+            await Task.Run(()=> {
+                int[] param = inParam as int[];
+
+                string url = "https://vk.com/doc";
+                int count = param[2] - param[1];
+
+                if (!Directory.Exists("docs/id" + param[0])) Directory.CreateDirectory("docs/id" + param[0]);
+
+                Invoke((MethodInvoker)(() =>
+                {
+                    progressDoc.Maximum = count;
+                }));
+
+                while (param[1] <= param[2])
+                {
+                    string doc = url + param[0] + "_" + param[1];
+                    string respnse = Get(doc);
+
+                    Invoke((MethodInvoker)(() =>
+                    {
+                        progressDoc.PerformStep();
+                        toolStripStatusLabel1.Text = "Проверка: " + doc;
+                    }));
+
+                    if (!respnse.Contains("/badbrowser.php"))
+                    {
+                        StreamWriter writer = new StreamWriter("docs/id" + param[0] + "/" + param[1] + ".html");
+                        writer.WriteLine(respnse);
+                        writer.Flush();
+                        writer.Close();
+                    }
+                    param[1]++;
+                }
+            });
+        }
+
+        public string Get(string url)
+        {
+            WebRequest webRequest = WebRequest.Create(url);
+            WebResponse response = webRequest.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            if (responseStream != null)
+            {
+                StreamReader streamReader = new StreamReader(responseStream);
+                string result = streamReader.ReadToEnd();
+                streamReader.Close();
+                return result;
+            }
+            return null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!Directory.Exists("docs"))
+            {
+                Directory.CreateDirectory("docs");
+            }
+        }
+    }
+}
